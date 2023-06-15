31a2a2576848c33c5977a90bf8bc5440da9367a3
thiagorabaioli <tfrabaioli@outlook.com>
2023-06-15 13:09:19 +0100

    first commit

---
+++ backend/src/main/java/tfr/dsmovies/DsmoviesApplication.java
@@ -0,0 +1,13 @@
+package tfr.dsmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tfr/dsmovies/DsmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package tfr.dsmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
